5680901121d1bc786729740e1c42939d07dbb0d2
Mehfooj <162735469+tsvlgd@users.noreply.github.com>
2026-02-10 07:46:11 +0000

    micrograd engine and nn class

---
+++ src/nn.py
@@ -0,0 +1,67 @@
+from src.engine import Value
+import random
+
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+class Neuron(Module):
+        def __init__(self, nin, nonlin=True,dropout=0.0):
+            self.w = [Value(random.uniform(-1,1))  for _ in nin]
+            self.b = Value(0)
+            self.nonlin = nonlin 
+            self.dropout = dropout
+
+        def __call__(self, x):
+            act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+            out = act.relu()
+
+            if self.dropout > 0 and random.random() < self.dropout:
+                return Value(0.0) # "pretend this neuron does not exist in this forward pass"
+
+
+            return out
+
+
+        def parameters(self):
+            return self.w + [self.b]
+
+        def __repr__(self):
+            return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+
+
+class Layer:
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]  
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+ 1], nonlin=i!= len(outs) - 1) for i in range (len(nouts))]
+
+
+    def __call__(self, x):
+        for layers in self.layers:
+            x = layers(x)
+        return x
+        
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
